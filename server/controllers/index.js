ae4873797a0bdca8f790affd98cf83b3c59138a2
Milen03 <milen.atanasovv03@gmail.com>
2026-07-29 20:20:31 +0300

    Add carControler to mains controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,10 @@
 const authController = require('./auth');
 const themeController = require('./themeController');
 const postController = require('./postController');
-
+const carController = require('./carController');
 module.exports = {
     authController,
     themeController,
     postController,
-}
+    carController
+};

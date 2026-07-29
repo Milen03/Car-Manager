a93cd30f4b56b82f72674573c82774d1223df5ee
Milen03 <milen.atanasovv03@gmail.com>
2026-07-29 20:15:04 +0300

    make funcion for get car by Id

--- server/controllers/carController.js
+++ server/controllers/carController.js
@@ -27,9 +27,20 @@ async function getAllCars(req, res) {
     }
 }
 
+async function getCarById(req,res){
+    try{
+        const car = await Car.findById(req.params.id);
+        if(!car) return res.status(404).json({error: 'Car not found'});
+        res.status(200).json(car);
+    }catch(error){
+        res.status(400).json({ error: error.message });
+    }
+}
+
 
 module.exports = {
     createCar,
-    getAllCars
+    getAllCars,
+    getCarById
 };
 

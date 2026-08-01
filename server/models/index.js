b82152817f3a5c0b2e47a5063ce6767cd2ce6e3f
Milen03 <milen.atanasovv03@gmail.com>
2026-08-01 14:49:57 +0300

    Add model to main models

--- server/models/index.js
+++ server/models/index.js
@@ -3,11 +3,13 @@ const tokenBlacklistModel = require('./tokenBlacklistModel');
 const themeModel = require('./themeModel');
 const postModel = require('./postModel');
 const carModel = require('./carModel');
+const serviceModel = require('./serviceModel');
 
 module.exports = {
     userModel,
     tokenBlacklistModel,
     themeModel,
     postModel,
-    carModel
+    carModel,
+    serviceModel
 }

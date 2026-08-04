e3eaf42fe89e863f27d5345f6d6d564c447db591
Milen03 <milen.atanasovv03@gmail.com>
2026-08-05 00:28:27 +0300

    Change db name

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3000,
-        dbURL: 'mongodb://localhost:27017/forum',
+        dbURL: 'mongodb://localhost:27017/Car-Manager',
         origin: ['http://localhost:5555', 'http://localhost:4200', 'http://localhost:5173']
     },
     production: {
